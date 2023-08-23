43c2cccc02935b70eeacfb1ce27bb71dab67689a
Bradley Hill <97234446+Bradley-Hill@users.noreply.github.com>
2023-08-23 15:03:52 +0200

    typesctipt set up completed

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,11 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
