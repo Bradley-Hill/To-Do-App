f44c3bd82b1f7e1c610689bd078d9a4b10b86cd4
Bradley Hill <97234446+Bradley-Hill@users.noreply.github.com>
2023-09-06 14:05:45 +0200

    TRying out typescript

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,31 @@ import './style.css';
 // This will be written in typescript from now on.
 // Strongly Typed is the only thing I know for now.
 
+// Define a type for the ToDoItem
+type ToDoItem = {
+    task: string;
+    description: string;
+    priority: number;
+    dueDate: Date;
+    isCompleted: boolean;
+    markComplete: () => void;
+  };
+
+  function createToDoItem(
+    task: string,
+    priority: number,
+    dueDate: Date,
+    description: string
+  ): ToDoItem {
+    const toDoItem: ToDoItem = {
+      task: task,
+      description: description,
+      priority: priority,
+      dueDate: dueDate,
+      isCompleted: false,
+      markComplete: function () {
+        this.isCompleted = true;
+      },
+    };
+    return toDoItem;
+  }
