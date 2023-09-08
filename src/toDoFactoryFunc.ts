d154c9051382db76be34f449996c213bbcae62da
Bradley Hill <97234446+Bradley-Hill@users.noreply.github.com>
2023-09-08 16:23:29 +0200

    added form functionality+toDoItem creation

---
+++ src/toDoFactoryFunc.ts
@@ -0,0 +1,27 @@
+// Define a type for the ToDoItem
+type ToDoItem = {
+
+    task: string;
+    description: string;
+    priority: number;
+    dueDate: Date;
+    isCompleted: boolean;
+    markComplete: () => void;
+  };
+
+  function createToDoItem(task: string,priority: number,dueDate: Date,description: string)
+  : ToDoItem {
+    const toDoItem: ToDoItem = {
+      task: task,
+      description: description,
+      priority: priority,
+      dueDate: dueDate,
+      isCompleted: false,
+      markComplete: function () {
+        this.isCompleted = true;
+      },
+    };
+    return toDoItem;
+  }
+
+  export {createToDoItem}
